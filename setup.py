408a705a5cb0a3d1989ca712db31813f2dca76ed
James Socol <james@mozilla.com>
2010-04-13 13:36:43 -0700

    Adding NoVarySessionMiddleware

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='commonware',
-    version='0.1.0',
+    version='0.2.0',
     description='A place to keep stuff we want to share, like middleware.',
     author='James Socol',
     author_email='james@mozilla.com',
