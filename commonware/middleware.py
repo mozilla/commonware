d52ee98d95f3ffa0d3114deedaf27f2f220aab24
Jeff Balogh <jbalogh@mozilla.com>
2010-09-10 15:31:21 -0700

    exception middleware to hide passwords in request.POST

--- commonware/middleware.py
+++ commonware/middleware.py
@@ -54,3 +54,16 @@ class FrameOptionsHeader(object):
             response['x-frame-options'] = 'DENY'
 
         return response
+
+
+class HidePasswordOnException(object):
+    """
+    Hide passwords in request.POST so they're not recorded in error logging.
+    """
+
+    def process_exception(self, request, exception):
+        # Get a copy so it's mutable.
+        request.POST = request.POST.copy()
+        for key in request.POST:
+            if 'password' in key.lower():
+                request.POST[key] = '******'
