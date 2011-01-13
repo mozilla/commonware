f07ae466e6aa8a6469913b95da60366358c1cbe4
James Socol <james@mozilla.com>
2011-01-13 10:40:52 -0500

    Create commonware.response

--- commonware/decorators.py
+++ commonware/decorators.py
@@ -1,27 +1,2 @@
-from functools import wraps
-
-from django.utils.decorators import available_attrs
-
-
-def xframe_sameorigin(view_fn):
-    def _wrapped_view(request, *args, **kwargs):
-        response = view_fn(request, *args, **kwargs)
-        response['x-frame-options'] = 'SAMEORIGIN'
-        return response
-    return wraps(view_fn, assigned=available_attrs(view_fn))(_wrapped_view)
-
-
-def xframe_allow(view_fn):
-    def _wrapped_view(request, *args, **kwargs):
-        response = view_fn(request, *args, **kwargs)
-        response.no_frame_options = True
-        return response
-    return wraps(view_fn, assigned=available_attrs(view_fn))(_wrapped_view)
-
-
-def xframe_deny(view_fn):
-    def _wrapped_view(request, *args, **kwargs):
-        response = view_fn(request, *args, **kwargs)
-        response['x-frame-options'] = 'DENY'
-        return response
-    return wraps(view_fn, assigned=available_attrs(view_fn))(_wrapped_view)
+from commonware.response.decorators import (xframe_sameorigin, xframe_allow,
+                                            xframe_deny)
--- commonware/middleware/__init__.py
+++ commonware/middleware/__init__.py
@@ -1,4 +1,4 @@
 from commonware.middleware.exceptions import HidePasswordOnException
 from commonware.middleware.request import SetRemoteAddrFromForwardedFor
-from commonware.middleware.response import FrameOptionsHeader
 from commonware.middleware.session import NoVarySessionMiddleware
+from commonware.response.middleware import FrameOptionsHeader
