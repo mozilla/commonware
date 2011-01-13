b8c9dfaf799a3e89cbdb03daf317f5f93cb5d823
James Socol <james@mozilla.com>
2011-01-13 10:46:37 -0500

    Create commonware.session

--- commonware/middleware/__init__.py
+++ commonware/middleware/__init__.py
@@ -1,5 +1,5 @@
 from commonware.log.middleware import ThreadRequestMiddleware
 from commonware.middleware.exceptions import HidePasswordOnException
 from commonware.middleware.request import SetRemoteAddrFromForwardedFor
-from commonware.middleware.session import NoVarySessionMiddleware
 from commonware.response.middleware import FrameOptionsHeader
+from commonware.session.middleware import NoVarySessionMiddleware
