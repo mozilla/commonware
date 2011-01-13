11360a579c061441b2bb3a76ab3929c46784067e
James Socol <james@mozilla.com>
2011-01-13 10:52:55 -0500

    Create commonware.exceptions

--- commonware/middleware/__init__.py
+++ commonware/middleware/__init__.py
@@ -1,5 +1,5 @@
+from commonware.exceptions.middleware import HidePasswordOnException
 from commonware.log.middleware import ThreadRequestMiddleware
-from commonware.middleware.exceptions import HidePasswordOnException
 from commonware.request.middleware import SetRemoteAddrFromForwardedFor
 from commonware.response.middleware import FrameOptionsHeader
 from commonware.session.middleware import NoVarySessionMiddleware
