eb536fd573783f324acb54be9654af1752bf2fbc
James Socol <james@mozilla.com>
2011-01-20 17:12:55 -0500

    Add HttpOnlyMiddleware and associated decorator.

--- commonware/response/decorators.py
+++ commonware/response/decorators.py
@@ -4,24 +4,38 @@ from django.utils.decorators import available_attrs
 
 
 def xframe_sameorigin(view_fn):
+    @wraps(view_fn, assigned=available_attrs(view_fn))
     def _wrapped_view(request, *args, **kwargs):
         response = view_fn(request, *args, **kwargs)
         response['x-frame-options'] = 'SAMEORIGIN'
         return response
-    return wraps(view_fn, assigned=available_attrs(view_fn))(_wrapped_view)
+    return _wrapped_view
 
 
 def xframe_allow(view_fn):
+    @wraps(view_fn, assigned=available_attrs(view_fn))
     def _wrapped_view(request, *args, **kwargs):
         response = view_fn(request, *args, **kwargs)
         response.no_frame_options = True
         return response
-    return wraps(view_fn, assigned=available_attrs(view_fn))(_wrapped_view)
+    return _wrapped_view
 
 
 def xframe_deny(view_fn):
+    @wraps(view_fn, assigned=available_attrs(view_fn))
     def _wrapped_view(request, *args, **kwargs):
         response = view_fn(request, *args, **kwargs)
         response['x-frame-options'] = 'DENY'
         return response
-    return wraps(view_fn, assigned=available_attrs(view_fn))(_wrapped_view)
+    return _wrapped_view
+
+
+def js_cookies(*cookies):
+    def decorator(view_fn):
+        @wraps(view_fn, assigned=available_attrs(view_fn))
+        def _wrapped_view(request, *args, **kwargs):
+            response = view_fn(request, *args, **kwargs)
+            response.js_cookies = cookies
+            return response
+        return _wrapped_view
+    return decorator
