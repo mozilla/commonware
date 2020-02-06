1ea0a69b366d3e9474c9f63eab514eb634aaabff
Ben Spaulding <ben@lincolnloop.com>
2020-02-06 16:45:35 -0700

    Remove import of removed class
    I missed this in commit  1dea5add.

--- commonware/middleware.py
+++ commonware/middleware.py
@@ -4,6 +4,5 @@ from commonware.request.middleware import SetRemoteAddrFromForwardedFor
 from commonware.response.middleware import (FrameOptionsHeader,
                                             ContentTypeOptionsHeader,
                                             RobotsTagHeader,
-                                            StrictTransportMiddleware,
                                             XSSProtectionHeader)
 from commonware.session.middleware import NoVarySessionMiddleware
